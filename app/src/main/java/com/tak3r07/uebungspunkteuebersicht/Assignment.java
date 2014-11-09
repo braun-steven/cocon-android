d3c49437b7f4e4d4df4b5ef05a17be1e467c7d77
Tak3r07 <Tak3r07@gmail.com>
2014-11-09 23:26:32 +0100

    new upload

---
+++ app/src/main/java/com/tak3r07/uebungspunkteuebersicht/Assignment.java
@@ -0,0 +1,63 @@
+package tak3r07.uebungspunkteuebersicht;
+
+import android.util.Log;
+
+/**
+ * Created by tak3r07 on 11/9/14.
+ */
+public class Assignment {
+
+    String TAG = "UEBUNGSPUNKTE-Log";
+
+    //Index of the Assignment
+    int index;
+
+    // Maximal reachable points of this assignment
+    double maxPoints;
+
+    //Achieved number of points of this assignment
+    double achievedPoints;
+
+    //Constructor
+    public Assignment(int index, double maxPoints, double achievedPoints){
+        setIndex(index);
+        setMaxPoints(maxPoints);
+        setAchievedPoints(achievedPoints);
+    }
+
+    //Calculates and return the achieved percentage of max points of this assignment
+    public double getPercentage(){
+        try{
+            return Math.round(achievedPoints/maxPoints*1000)/1000;
+        } catch (Exception e){
+            e.printStackTrace();
+            return 0;
+        }
+
+    }
+
+
+    public double getMaxPoints() {
+        return maxPoints;
+    }
+
+    public void setMaxPoints(double maxPoints) {
+        this.maxPoints = maxPoints;
+    }
+
+    public double getAchievedPoints() {
+        return achievedPoints;
+    }
+
+    public void setAchievedPoints(double achievedPoints) {
+        this.achievedPoints = achievedPoints;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+}
---
+++ app/src/main/java/com/tak3r07/uebungspunkteuebersicht/Course.java
@@ -0,0 +1,70 @@
+package tak3r07.uebungspunkteuebersicht;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by tak3r07 on 11/9/14.
+ */
+public class Course {
+
+    public Course(String courseName) {
+        setCourseName(courseName);
+    }
+
+    //Coursename
+    private String courseName;
+
+    //List of the course assignments
+    private ArrayList<Assignment> mAssignmentArrayList = new ArrayList<Assignment>();
+
+
+    //Constructor
+    public Assignment getAssignment(int index) {
+        try {
+            return mAssignmentArrayList.get(index);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    //Add new Course
+    public void addAssignment(Assignment assignment) {
+        mAssignmentArrayList.add(assignment);
+    }
+
+    //Calculate overall percentage
+    public double getOverAllPercentage() {
+        double overAllAchievedPoints = 0;
+        double overAllMaxPoints = 0;
+
+
+        //Iterate on the array and sum up all its max and achieved points
+        Assignment currentAssignment;
+        for (Iterator<Assignment> iterator = mAssignmentArrayList.iterator(); iterator.hasNext(); ) {
+            currentAssignment = iterator.next();
+            overAllAchievedPoints += currentAssignment.getAchievedPoints();
+            overAllMaxPoints += currentAssignment.getMaxPoints();
+        }
+
+        //Round on 4 digits
+        double overAllPercentage = Math.round(overAllAchievedPoints / overAllMaxPoints * 1000) / 1000;
+
+        //return result
+        return overAllPercentage;
+    }
+
+
+    public ArrayList<Assignment> getmAssignmentArrayList() {
+        return mAssignmentArrayList;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+}
