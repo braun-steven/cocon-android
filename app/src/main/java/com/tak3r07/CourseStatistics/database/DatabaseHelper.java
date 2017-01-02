bc6a9017561f6c7f1b890beb4386fd7f25f0e915
Steven Lang <slang03@students.uni-mainz.de>
2017-01-02 16:36:59 +0100

    Extract static strings from DatabaseHelper

--- app/src/main/java/com/tak3r07/CourseStatistics/database/DatabaseHelper.java
+++ app/src/main/java/com/tak3r07/CourseStatistics/database/DatabaseHelper.java
@@ -14,6 +14,7 @@ import com.tak3r07.CourseStatistics.objects.FixedPointsCourse;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import static com.tak3r07.CourseStatistics.database.DatabaseVocab.*;
 
 /**
  * Created by tak3r07 on 5/5/15.
@@ -29,51 +30,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     //Database name
     private static final String DATABASE_NAME = "courses";
 
-    //Table names
-    private static final String TABLE_COURSES = "courses";
-    private static final String TABLE_ASSIGNMENTS = "assignments";
 
-    //Column names: common
-    public static final String KEY_ID = "id";
-    public static final String KEY_DATE = "date";
-    public static final String KEY_ASSIGNMENTS = "assignments";
-
-    //Column names: courses
-    public static final String KEY_COURSENAME = "course_name";
-    public static final String KEY_NUMBER_OF_ASSIGNMENTS = "number_of_assignments";
-    public static final String KEY_REACHABLE_POINTS_PER_ASSIGNMENT = "reachable_points_per_assignment";
-    public static final String KEY_COURSE_INDEX = "course_index";
-    public static final String KEY_HAS_FIXED_POINTS = "has_fixed_points";
-    public static final String KEY_NEC_PERCENT_TO_PASS = "nec_percent_to_pass";
-
-    //Column names: assignments
-    public static final String KEY_ASSIGNMENT_INDEX = "assignment_index";
-    public static final String KEY_MAX_POINTS = "max_points";
-    public static final String KEY_ACHIEVED_POINTS = "achieved_points";
-    public static final String KEY_IS_EXTRA_ASSIGNMENT = "is_extra_assignment";
-    public static final String KEY_COURSE_ID = "course_id";
-
-
-    //Table create statements
-    private static final String CREATE_COURSE_TABLE = "CREATE TABLE " + TABLE_COURSES + "("
-            + KEY_ID + " TEXT PRIMARY KEY,"
-            + KEY_COURSENAME + " TEXT,"
-            + KEY_NUMBER_OF_ASSIGNMENTS + " INTEGER,"
-            + KEY_REACHABLE_POINTS_PER_ASSIGNMENT + " REAL,"
-            + KEY_COURSE_INDEX + " INTEGER,"
-            + KEY_HAS_FIXED_POINTS + " INTEGER,"
-            + KEY_NEC_PERCENT_TO_PASS + " REAL DEFAULT 0.5,"
-            + KEY_DATE + " INTEGER DEFAULT 0)";
-
-    private static final String CREATE_ASSIGNMENT_TABLE = "CREATE TABLE " + TABLE_ASSIGNMENTS + "("
-            + KEY_ID + " TEXT PRIMARY KEY,"
-            + KEY_ASSIGNMENT_INDEX + " INTEGER,"
-            + KEY_MAX_POINTS + " REAL,"
-            + KEY_IS_EXTRA_ASSIGNMENT + " INTEGER,"
-            + KEY_COURSE_ID + " TEXT,"
-            + KEY_ACHIEVED_POINTS + " REAL,"
-            + KEY_DATE + " INTEGER DEFAULT 0,"
-            + "FOREIGN KEY(" + KEY_COURSE_ID + ") REFERENCES " + TABLE_COURSES + "(" + KEY_COURSE_ID + "))";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
